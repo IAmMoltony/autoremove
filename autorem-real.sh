7e35dcb0921a5979b79f97f5f755ea3b3d6f73de
IAmMoltony <koronavirusnyj@gmail.com>
2024-02-23 15:20:50 +0600

    now it works

--- autorem-real.sh
+++ autorem-real.sh
@@ -1,4 +1,4 @@
-#!/bin/sh
+#!/usr/bin/env bash
 
 notify-send "******* Starting autoremove..."
 apt autoremove -y | while IFS= read -r line; do notify-send "$line"; done
--- autorem.sh
+++ autorem.sh
@@ -1,3 +1,4 @@
-#!/bin/sh
+#!/usr/bin/env bash
 
-/home/moltony/scripts/autorem-real.sh &
+cd $(dirname $0)
+./autorem-real.sh &
