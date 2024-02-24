7270eb04d43bed237185b353274fc8cc975a0d65
IAmMoltony <koronavirusnyj@gmail.com>
2024-02-24 20:40:48 +0600

    Fix "waiting for package lock" notifications

--- autorem-real.sh
+++ autorem-real.sh
@@ -1,5 +1,8 @@
 #!/usr/bin/env bash
 
+# Sleep for a few seconds so the screen doesn't get flooded with "Waiting for package lock ..." notifications
+sleep 10
+
 notify-send "******* Starting autoremove..."
 apt autoremove -y | while IFS= read -r line; do notify-send "$line"; done
 notify-send "******* Autoremove finished"
